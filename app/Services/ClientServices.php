3ef8fc894e10a873682a4ff669d40076156101d5
akahard13 <harnaldosanchez49@gmail.com>
2025-07-17 15:35:02 -0600

    correccion al commit anterior

--- app/Services/ClientServices.php
+++ app/Services/ClientServices.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Catalogs\JobPositions;
 use App\Models\Clients;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class ClientServices
@@ -15,7 +16,9 @@ class ClientServices
             ->leftJoin('system.users as u', function ($join) {
                 $join->on('u.reference_id', '=', 'c.id')
                     ->where('u.table_reference', '=', 'client');
-            })->select('c.*', 'gen.name as gender', DB::raw('case when u.id is not null and u.deleted=false and u.active=true then true else false end as is_active'))->get();
+            })->select('c.*', 'gen.name as gender', DB::raw('case when u.id is not null and u.deleted=false and u.active=true then true else false end as is_active'))
+            ->where('c.deleted', false)
+            ->where('c.active', true)->get();
     }
 
     public function update($request, $id)
