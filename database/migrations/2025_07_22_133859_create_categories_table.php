616c7656587baba927fcd964b79d04140412838c
akahard13 <harnaldosanchez49@gmail.com>
2025-07-25 14:24:28 -0600

    se hacen insert de categorias desde la migracion

--- database/migrations/2025_07_22_133859_create_categories_table.php
+++ database/migrations/2025_07_22_133859_create_categories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -20,6 +21,10 @@ return new class extends Migration
             $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
+        DB::table('catalogs.categories')->insert([
+            ['name' => 'Planes', 'outcoming' => false, 'incoming' => true, 'active' => true, 'deleted' => false],
+            ['name' => 'Salarios', 'outcoming' => true, 'incoming' => false, 'active' => true, 'deleted' => false],
+        ]);
     }
 
     /**
