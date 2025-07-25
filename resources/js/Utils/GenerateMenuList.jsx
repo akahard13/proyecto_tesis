cb996ae6b33b7ef2fcc5f11ec2c003682c7e2dab
akahard13 <harnaldosanchez49@gmail.com>
2025-07-25 13:42:01 -0600

    Mejora en el menu, se excluyen las rutas relacionadas a los pagos para que no haya errores sin el id necesario

--- resources/js/Utils/GenerateMenuList.jsx
+++ resources/js/Utils/GenerateMenuList.jsx
@@ -1,13 +1,20 @@
 export function GenerateMenuList(list) {
+    const excludedPrefixes = ['payments_plans', 'payments_employees'];
+
     const viewPermissions = list
-        .filter(l => l.permission_name.endsWith('.view'))
+        .filter(l =>
+            l.permission_name.endsWith('.view') &&
+            !excludedPrefixes.includes(l.permission_name.split('.')[0])
+        )
         .map(l => ({
             prefix: l.permission_name.split('.')[0],
             page_name: l.page_name,
         }));
+
     const uniqueViewPermissions = viewPermissions.filter(
         (li, index, self) =>
             index === self.findIndex(l => l.prefix === li.prefix)
     );
+
     return uniqueViewPermissions;
 }
