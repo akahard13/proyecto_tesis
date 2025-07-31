82e448b6e6c4cebfeaa792551fdf1b5247fcb4b8
Ivantm1425 <it1498242@gmail.com>
2025-07-31 11:47:09 -0600

    se modifica el wrap para impresiones

--- resources/js/Components/Wrap.jsx
+++ resources/js/Components/Wrap.jsx
@@ -3,21 +3,23 @@ import { Head } from "@inertiajs/react";
 
 export default function Wrap({ children, header }) {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Permisos
-                </h2>
-            }
-        >
-            <Head title={header} />
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-factor-secundary p-6 shadow rounded-lg">
-                        {children}
+        <div className="print:hidden">
+            <AuthenticatedLayout
+                header={
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                        Permisos
+                    </h2>
+                }
+            >
+                <Head title={header} />
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-factor-secundary p-6 shadow rounded-lg">
+                            {children}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </AuthenticatedLayout>
+            </AuthenticatedLayout>
+        </div>
     );
 }
